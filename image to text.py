41616f54c9e238b878a1be9865ea7329e910aa1b
raffeemdai <63146105+raffeemdai@users.noreply.github.com>
2020-05-26 23:13:53 -0700

    Add files via upload
    HI

---
+++ image to text.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+pip install pytesseract
+
+
+# In[7]:
+
+
+pip install pyttsx3
+
+
+# In[2]:
+
+
+import io
+import requests
+import pytesseract
+from PIL import Image, ImageFilter, ImageEnhance
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\ADMIN\AppData\Local\Tesseract-OCR\tesseract.exe"
+
+
+# In[8]:
+
+
+pip install googletrans
+
+
+# In[5]:
+
+
+import io
+import requests
+import pytesseract
+from PIL import Image, ImageFilter, ImageEnhance
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\ADMIN\AppData\Local\Tesseract-OCR\tesseract.exe"
+response = requests.get('https://chrome.google.com/webstore/detail/address-verification-find/eaagjaekeihloinhcefkmciimoahnmld')
+
+
+# In[5]:
+
+
+# import the following libraries 
+# will convert the image to text string 
+import pytesseract       
+  
+# adds image processing capabilities 
+from PIL import Image     
+  
+ # converts the text to speech   
+import pyttsx3            
+  
+#translates into the mentioned language 
+from googletrans import Translator       
+  
+ # opening an image from the source path 
+img = Image.open('C:\Users\s777593\Desktop\hi.png')      
+  
+
+
+# In[13]:
+
+
+# import the following libraries 
+# will convert the image to text string 
+import pytesseract       
+  
+# adds image processing capabilities 
+from PIL import Image     
+  
+ # converts the text to speech   
+import pyttsx3            
+  
+#translates into the mentioned language 
+from googletrans import Translator       
+  
+ # opening an image from the source path 
+img = Image.open(r'C:\Users\s777593\Desktop\text15.png') 
+print(img) 
+##C:\Users\s777593\AppData\Local\Continuum\anaconda3\Lib\site-packages\pytesseract
+pytesseract.pytesseract.tesseract_cmd =r'C:\Users\s777593\AppData\Local\Continuum\anaconda3\Lib\site-packages\pytesseract\pytesseract.py'
+
+result = pytesseract.image_to_string(img) 
+
+
+# In[6]:
+
+
+--paths
+
+
+# In[18]:
+
+
+pip install tesseract
+##pip install tesseract-OCR
+
+
+# In[14]:
+
+
+# import the following libraries 
+# will convert the image to text string 
+import pytesseract       
+  
+# adds image processing capabilities 
+from PIL import Image     
+  
+ # converts the text to speech   
+import pyttsx3            
+  
+#translates into the mentioned language 
+from googletrans import Translator       
+  
+ # opening an image from the source path 
+img = Image.open(r'C:\Users\s777593\Desktop\text15.png') 
+print(img) 
+##C:\Users\s777593\AppData\Local\Continuum\anaconda3\Lib\site-packages\pytesseract
+pytesseract.pytesseract.tesseract_cmd =r'C:\Users\s777593\AppData\Local\Continuum\anaconda3\Lib\site-packages\pytesseract\pytesseract.py'
+
+result = pytesseract.image_to_string(img) 
+
+
+# In[1]:
+
+
+pip install tesseract-ocr
+
+
+# In[20]:
+
+
+pip install --upgrade setuptools
+
+
+# In[3]:
+
+
+pip install tesseract-ocr
+
+
+# In[4]:
+
+
+pip install tesseract-ocr
+
+
+# In[ ]:
+
+
+
+
